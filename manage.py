f39e42eb9fe5fbd937fc82f5d6975710bc200086
Justin Velluppillai <justinvelluppillai@gmail.com>
2013-03-31 21:07:52 +1300

    Update manage.py

--- manage.py
+++ manage.py
@@ -3,7 +3,7 @@ import os
 import sys
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "{{ project_name }}.setting")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "website.setting")
 
     from django.core.management import execute_from_command_line
 
